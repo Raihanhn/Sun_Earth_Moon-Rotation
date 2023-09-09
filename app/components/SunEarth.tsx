6b3f2bc889cd85c9ef575a4319c1bf8dfa06507d
Raihan <raihanhn20@gmail.com>
2023-09-09 16:36:35 +0600

    first commit

---
+++ app/components/SunEarth.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SunEarth() {
+  return (
+    <div>
+      <div className="center">
+        <div className="sun">
+          <div className="earth">
+            <div className="moon"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
